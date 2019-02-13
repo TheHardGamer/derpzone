BlissRoms/platform_packages_apps_Blissify

Create QS header pref screen in quic settings tab
Change-Id: I23cfbb01e7c2db11a9a8cc6aac672f475d169855

src/com/blissroms/blissify/fragments/qs/Header.java
@@ -1,0 +1,64 @@
+package com.blissroms.blissify.fragments.qs;
+
+import android.app.AlertDialog;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnCancelListener;
+import android.content.res.Resources;
+import android.database.ContentObserver;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.os.UserHandle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceScreen;
+import android.support.v7.preference.ListPreference;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.text.format.DateFormat;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import com.blissroms.blissify.preference.SystemSettingSwitchPreference;
+import com.blissroms.blissify.preference.SystemSettingSeekBarPreference;
+import android.provider.Settings;
+
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+
+import com.android.settings.bliss.preference.ColorPickerPreference;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.blissroms.blissify.preference.SystemSettingSwitchPreference;
+import com.android.internal.logging.nano.MetricsProto;
+
+public class Header extends SettingsPreferenceFragment 
+             implements Preference.OnPreferenceChangeListener{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+            addPreferencesFromResource(R.xml.qs_header);
+            PreferenceScreen prefSet = getPreferenceScreen();
+            ContentResolver resolver = getActivity().getContentResolver();
+
+        }
+
+        public boolean onPreferenceChange(Preference preference, Object newValue) {
+            ContentResolver resolver = getActivity().getContentResolver();
+        return false;
+        }
+
+        @Override
+        public int getMetricsCategory() {
+            return MetricsProto.MetricsEvent.BLISSIFY;
+        }
+}

TheHardGamer/derpzone

src/com/derpcaf/derpzone/qs/Header.java
@@ -1,0 +1,64 @@
+package com.derpcaf.derpzone.fragments.qs;
+
+import android.app.AlertDialog;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnCancelListener;
+import android.content.res.Resources;
+import android.database.ContentObserver;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.os.UserHandle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceScreen;
+import android.support.v7.preference.ListPreference;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.text.format.DateFormat;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import com.blissroms.blissify.preference.SystemSettingSwitchPreference;
+import com.blissroms.blissify.preference.SystemSettingSeekBarPreference;
+import android.provider.Settings;
+
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+
+import com.android.settings.derpcaf.derpzone.preference.ColorPickerPreference;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.derpcaf.derpzone.preference.SystemSettingSwitchPreference;
+import com.android.internal.logging.nano.MetricsProto;
+
+public class Header extends SettingsPreferenceFragment 
+             implements Preference.OnPreferenceChangeListener{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+            addPreferencesFromResource(R.xml.qs_header);
+            PreferenceScreen prefSet = getPreferenceScreen();
+            ContentResolver resolver = getActivity().getContentResolver();
+
+        }
+
+        public boolean onPreferenceChange(Preference preference, Object newValue) {
+            ContentResolver resolver = getActivity().getContentResolver();
+        return false;
+        }
+
+        @Override
+        public int getMetricsCategory() {
+            return MetricsProto.MetricsEvent.DERPZONE;
+        }
+}