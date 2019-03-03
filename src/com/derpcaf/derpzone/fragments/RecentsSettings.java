Project-Xtended/packages_apps_Xtensions

Allow users to switch between Pie/Oreo recents [2/3]

src/com/msm/xtended/fragments/RecentsSettings.java
@@ -19,6 +19,7 @@ import com.android.settings.R;
 import java.util.Arrays;
 import java.util.HashSet;
 
+import com.android.internal.util.xtended.XtendedUtils;
 import com.android.settings.SettingsPreferenceFragment;
 
 public class RecentsSettings extends SettingsPreferenceFragment implements
@@ -26,7 +27,9 @@ public class RecentsSettings extends SettingsPreferenceFragment implements
 
     private static final String RECENTS_CLEAR_ALL_LOCATION = "recents_clear_all_location";
     private static final String IMMERSIVE_RECENTS = "immersive_recents";
+    private static final String RECENTS_COMPONENT_TYPE = "recents_component";
 
+    private ListPreference mRecentsComponentType;
     private ListPreference mImmersiveRecents;
     private ListPreference mRecentsClearAllLocation;
     private SwitchPreference mRecentsClearAll;
@@ -37,7 +40,7 @@ public class RecentsSettings extends SettingsPreferenceFragment implements
 
 	mFooterPreferenceMixin.createFooterPreference()
 		.setTitle(R.string.dumb_dev);
-	
+
         addPreferencesFromResource(R.xml.x_settings_recents);
 
         ContentResolver resolver = getActivity().getContentResolver();
@@ -55,6 +58,14 @@ public class RecentsSettings extends SettingsPreferenceFragment implements
                 resolver, Settings.System.IMMERSIVE_RECENTS, 0)));
         mImmersiveRecents.setSummary(mImmersiveRecents.getEntry());
         mImmersiveRecents.setOnPreferenceChangeListener(this);
+
+        // recents component type
+        mRecentsComponentType = (ListPreference) findPreference(RECENTS_COMPONENT_TYPE);
+        int type = Settings.System.getInt(resolver,
+                Settings.System.RECENTS_COMPONENT, 0);
+        mRecentsComponentType.setValue(String.valueOf(type));
+        mRecentsComponentType.setSummary(mRecentsComponentType.getEntry());
+        mRecentsComponentType.setOnPreferenceChangeListener(this);
     }
 
     @Override
@@ -72,6 +83,18 @@ public class RecentsSettings extends SettingsPreferenceFragment implements
                     Integer.valueOf((String) objValue));
             mImmersiveRecents.setValue(String.valueOf(objValue));
             mImmersiveRecents.setSummary(mImmersiveRecents.getEntry());
+            return true;
+        } else if (preference == mRecentsComponentType) {
+            int type = Integer.valueOf((String) objValue);
+            int index = mRecentsComponentType.findIndexOfValue((String) objValue);
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.RECENTS_COMPONENT, type);
+            mRecentsComponentType.setSummary(mRecentsComponentType.getEntries()[index]);
+            if (type == 1) { // Disable swipe up gesture, if oreo type selected
+               Settings.Secure.putInt(getActivity().getContentResolver(),
+                    Settings.Secure.SWIPE_UP_TO_SWITCH_APPS_ENABLED, 0);
+            }
+            XtendedUtils.showSystemUiRestartDialog(getContext());
             return true;
         }
         return false;

TheHardGamer/derpzone

src/com/derpcaf/derpzone/fragments/RecentsSettings.java
@@ -19,6 +19,7 @@ import com.android.settings.R;
 import java.util.Arrays;
 import java.util.HashSet;
 
+import com.android.internal.util.derpcaf.DerpcafUtils;
 import com.android.settings.SettingsPreferenceFragment;
 
 public class RecentsSettings extends SettingsPreferenceFragment implements
@@ -26,7 +27,9 @@ public class RecentsSettings extends SettingsPreferenceFragment implements
 
     private static final String RECENTS_CLEAR_ALL_LOCATION = "recents_clear_all_location";
     private static final String IMMERSIVE_RECENTS = "immersive_recents";
+    private static final String RECENTS_COMPONENT_TYPE = "recents_component";
 
+    private ListPreference mRecentsComponentType;
     private ListPreference mImmersiveRecents;
     private ListPreference mRecentsClearAllLocation;
     private SwitchPreference mRecentsClearAll;
@@ -52,6 +55,14 @@ public class RecentsSettings extends SettingsPreferenceFragment implements
                 resolver, Settings.System.IMMERSIVE_RECENTS, 0)));
         mImmersiveRecents.setSummary(mImmersiveRecents.getEntry());
         mImmersiveRecents.setOnPreferenceChangeListener(this);
+
+        // recents component type
+        mRecentsComponentType = (ListPreference) findPreference(RECENTS_COMPONENT_TYPE);
+        int type = Settings.System.getInt(resolver,
+                Settings.System.RECENTS_COMPONENT, 0);
+        mRecentsComponentType.setValue(String.valueOf(type));
+        mRecentsComponentType.setSummary(mRecentsComponentType.getEntry());
+        mRecentsComponentType.setOnPreferenceChangeListener(this);
     }
 
     @Override
@@ -69,6 +80,18 @@ public class RecentsSettings extends SettingsPreferenceFragment implements
                     Integer.valueOf((String) objValue));
             mImmersiveRecents.setValue(String.valueOf(objValue));
             mImmersiveRecents.setSummary(mImmersiveRecents.getEntry());
+            return true;
+        } else if (preference == mRecentsComponentType) {
+            int type = Integer.valueOf((String) objValue);
+            int index = mRecentsComponentType.findIndexOfValue((String) objValue);
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.RECENTS_COMPONENT, type);
+            mRecentsComponentType.setSummary(mRecentsComponentType.getEntries()[index]);
+            if (type == 1) { // Disable swipe up gesture, if oreo type selected
+               Settings.Secure.putInt(getActivity().getContentResolver(),
+                    Settings.Secure.SWIPE_UP_TO_SWITCH_APPS_ENABLED, 0);
+            }
+            DerpcafUtils.showSystemUiRestartDialog(getContext());
             return true;
         }
         return false;